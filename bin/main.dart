2dfc087435071bec00cc73a9d977e0448b4ced5f
mcclayac <anthony.mcclay@me.com>
2020-08-07 15:07:44 -0400

    Part of the chapter 7.12

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+
+
+//code 7.12
+import 'dart:collection';  // we are about to import some extra methods from collection library
+
+
 void main(List<String> arguments) {
   print("----------------------------------------------");
   print("----------------------------------------------");
@@ -17,8 +23,15 @@ void main(List<String> arguments) {
   code7_5();
   // Set: An Unordered Collections of Unique Items
   code7_6();
-
-
+  // Maps: The Key-Value Pair
+  code7_7();
+  // Using Collections Together
+  code7_8();
+  code7_9();
+  code7_10();
+  code7_11();
+  //  Queue Is Open-Ended
+  code7_12();
 
 
 
@@ -26,10 +39,220 @@ void main(List<String> arguments) {
 
 }
 
+
 //----------------------------------------------
 //----------------------------------------------
 
+//code 7.12
+//import 'dart:collection';  // we are about to import some extra methods from collection library
 
+void code7_12() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.12");
+  print("");
+
+  Queue myQueue = new Queue();
+  print("Default implementation ${myQueue.runtimeType}");
+  myQueue.add("Sanjib");
+  myQueue.add(54);
+  myQueue.add("Howrah");
+  myQueue.add("sanjib12sinha@gmail.com");
+  for(var allTheValues in myQueue){
+    print(allTheValues);
+  }
+  print("----------");
+  print("We are removing the first element ${myQueue.elementAt(0)}.");
+  myQueue.removeFirst();
+  for(var allTheValues in myQueue){
+    print(allTheValues);
+  }
+  print("----------");
+  print("We are removing the last element ${myQueue.elementAt(2  )}.");
+  myQueue.removeLast();
+  for(var allTheValues in myQueue){
+    print(allTheValues);
+  }
+
+}
+
+//----------------------------------------------
+//----------------------------------------------
+
+void code7_11() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.11");
+  print("");
+  var name;
+  var age;
+  List<Map<String, dynamic>> users = [
+    { name: "Peter", age: 18 },
+    { name: "Mira", age: 20 },
+    { name: "Jason", age: 22 },
+    { name: "Morgan", age: 32 },
+    { name: "Mary", age: 50 },
+    { name: "Will", age: 86 },
+    { name: "Bruce", age: 96 },
+  ];
+  var isEighteenAndOver = users.every((user) => user[age] >= 18);
+  print(isEighteenAndOver);
+  var hasNamesWithLetterA = users.every((user) => user.toString().startsWith("A"));
+  print(hasNamesWithLetterA);
+  var overTwentyOne = users.where((user) => user[age] > 21);
+  print(overTwentyOne.length);
+  print(overTwentyOne);
+  var underEighteen = users.singleWhere((user) => user[age] < 18, orElse: () => null);
+  print(underEighteen);
+}
+
+//----------------------------------------------
+//----------------------------------------------
+
+void code7_10() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.10");
+  print("");
+
+  var name;
+  var age;
+  List<Map<String, dynamic>> users = [
+    { name: "Peter", age: 18 },
+    { name: "Mira", age: 20 },
+    { name: "Jason", age: 22 },
+    { name: "Morgan", age: 32 },
+    { name: "Mary", age: 50 },
+    { name: "Will", age: 86 },
+    { name: "Bruce", age: 96 },
+  ];
+  var isEighteenAndOver = users.every((user) => user[age] >= 18);
+  print(isEighteenAndOver);
+  var hasNamesWithLetterA = users.every((user) => user.toString().startsWith("A"));
+  print(hasNamesWithLetterA);
+  var overTwentyOne = users.where((user) => user[age] > 21);
+  print(overTwentyOne.length);
+  print(overTwentyOne);
+
+
+}
+//----------------------------------------------
+//----------------------------------------------
+
+//code 7.9
+
+void code7_9() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.9");
+  print("");
+
+  var name;
+  var age;
+  List<Map<String, dynamic>> users = [
+    { name: "APeter", age: 18 },
+    { name: "AMira", age: 20 },
+    { name: "Anthony", age: 50 },
+    { name: "AJason", age: 22 },
+  ];
+  var isEighteenAndOver = users.every((user) => user[age] >= 18);
+  print(isEighteenAndOver);
+  var hasNamesWithLetterA = users.every((user) => user.toString().startsWith("A"));
+  print(hasNamesWithLetterA);
+}
+//----------------------------------------------
+//----------------------------------------------
+
+void code7_8() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.8");
+  print("");
+  var name;
+  var age;
+  List<Map<String, dynamic>> users = [
+    { name: "Peter", age: 18 },
+    { name: "Mira", age: 20 },
+//    { name: "Jason", age: 6 },
+    { name: "Tony", age: 22 },
+  ];
+  var is18AndOver = users.every((user) => user[age] >= 18);
+  print(is18AndOver);
+}
+//----------------------------------------------
+//----------------------------------------------
+
+//code 7.7
+void mapFunction(){
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.7");
+  print("");
+  //unordered collection of key=>value pair
+  Map<String, String> countries = Map();
+  countries['India'] = "Asia";
+  countries["Germany"] = "Europe";
+  countries["France"] = "Europe";
+  countries["Brazil"] = "South America";
+  //1. method we can obtain key or value
+  for(var key in countries.keys){
+    print("Country's name: $key");
+  }
+  print("-----------");
+  for(String value in countries.values){
+    print("Continent's name: $value");
+  }
+  //2. method
+  countries.forEach((key, value) => print("Country: $key and Continent: $value"));
+  //we can update any map very easily
+  if(countries.containsKey("Germany")){
+    countries.update("Germany", (value) => "European Union");
+    print("Updated country Germany.");
+    countries.forEach((key, value) => print("Country: $key and Continent: $value"));
+  }
+  //we can remove any country
+  countries.remove("Brazil");
+  countries.forEach((key, value) => print("Country: $key and Continent: $value"));
+  print("Barzil has been removed successfully.");
+  print("-----------");
+  //3. method of creating a map
+  Map<String, int> telephoneNumbersOfCustomers = {
+    "John" : 1234,
+    "Mac" : 7534,
+    "Molly" : 8934,
+    "Plywod" : 1275,
+    "Hagudu" : 2534
+  };
+  telephoneNumbersOfCustomers.forEach((key, value) => print("Customer: $key and Contact Number: $value"));
+}
+
+void code7_7() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.7");
+  print("");
+  mapFunction();
+}
+
+//----------------------------------------------
+//----------------------------------------------
+
+void codeSeg1() {
+  Map<String,String> countries = Map();
+    countries["India"] = "Asia";
+    countries["Germany"] = "Europe";
+    countries["France"] = "Europe";
+    countries["Brazil"] = "South America";
+    countries["USA"] = "North America";
+
+  Map<String,int> telephoneNumOfCust = {
+    "John" : 1234,
+    "Mac" : 456,
+    "Molly" : 789,
+    "Plywod" : 135,
+    "Hagudu" : 246
+  };
+}
 //code 7.6
 void setFunction(){
   //set is an unordered collections of unique items
@@ -68,6 +291,10 @@ void setFunction(){
 
 
 void code7_6() {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("Code 7.6");
+  print("");
   setFunction();
 }
 
