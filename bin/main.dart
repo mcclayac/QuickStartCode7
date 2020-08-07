33b6d3e56d97156d5eaea10cb2bdfc45a649465d
mcclayac <anthony.mcclay@me.com>
2020-08-07 10:07:10 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print("----------------------------------------------");
+  print('Dart Quick Start Chapter 7');
+  print('7. Data Structures and Collections');
+  // https://learning.oreilly.com/library/view/quick-start-guide/9781484255629/A490744_1_En_7_Chapter.html
+
+
+}
